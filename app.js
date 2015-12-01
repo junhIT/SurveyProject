e6cfaee1b77ada39835ceb1aab327f8d7710ab0c
5441-34 <5441-34>
2015-12-01 14:41:49 +0900

    호옹이

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var surveys = require('./routes/surveys');
 
 var app = express();
 // https://blooming-wave-1484.herokuapp.com/
--- routes/surveys.js
+++ routes/surveys.js
@@ -1,2 +1,9 @@
 var express = require('express');
 var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.redirect('/users/');
+});
+
+module.exports = router;
